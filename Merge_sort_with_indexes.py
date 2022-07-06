002e695c22c7e2f9c88ddc997e4e34f9afd6f4f2
AlbinaKaybysheva <kaybysheva.a@mail.ru>
2022-07-06 17:28:56 +0300

    Merge sort with indexes changed

--- Merge_sort_with_indexes.py
+++ Merge_sort_with_indexes.py
@@ -4,53 +4,60 @@
 
 
 from math import log
-from math import ceil
+from math import floor
 
-def Split(list, l, r, Q):
-    if r >= l:
-        if l == r:
-            Q.append([list[l]])
-        else:
-            Split(list, l, (l + r) // 2, Q)
-            Split(list, (l + r) // 2 + 1, r, Q)
-    return Q
+# def Split(list, l, r, Q):
+#     if r >= l:
+#         if l == r:
+#             Q.append([list[l]])
+#         else:
+#             Split(list, l, (l + r) // 2, Q)
+#             Split(list, (l + r) // 2 + 1, r, Q)
+#     return Q
+#
+# def MergeAll(que):
+#     i = 0
+#     while i < len(que) - 1:
+#         que += [Merge(que[i], que[i + 1])]
+#         i += 2
+#     return que[len(que) - 1]
 
-def MergeAll(que):
-    i = 0
-    while i < len(que) - 1:
-        que += [Merge(que[i], que[i + 1])]
-        i += 2
-    return que[len(que) - 1]
-
-def Merge(list1, list2):
+def Merge(A, p, q, r):
+    p -= 1 # привожу номер элемента массива к виду согласно примеру запуска
+    q -= 1 # (нумерация элементов начинается с 1)
+    r -= 1
     merged_list = []
-    stop_sign = len(list1) + len(list2)
-    i1, i2 = 0, 0
+    stop_sign = r - p + 1
+    i1, i2 = p, q + 1
     while len(merged_list) < stop_sign:
-        if i1 == len(list1):
-            merged_list += list2[i2:]
-        elif i2 == len(list2):
-            merged_list += list1[i1:]
+        #print(merged_list)
+        if i1 == q + 1:
+            merged_list += A[i2:r + 1]
+        elif i2 == r + 1:
+            merged_list += A[i1:q + 1]
         else:
-            if list1[i1] <= list2[i2]:
-                merged_list.append(list1[i1])
+            if A[i1] <= A[i2]:
+                merged_list.append(A[i1])
                 i1 += 1
-            elif list1[i1] > list2[i2]:
-                merged_list.append(list2[i2])
+            elif A[i1] > A[i2]:
+                merged_list.append(A[i2])
                 i2 += 1
-    return merged_list
+    A[p:r + 1] = merged_list
+    return A
 
-def Sort(list):
-    n1 =2 ** ceil(log(len(list), 2))
-    l1= n1 - len(list)
-    list = [0 for i in range(n1-len(list))] + list
-    Q = []
-    return MergeAll(Split(list, 0, len(list) - 1, Q))[l1:]
+def Sort(list, p, r):
+    if p < r:
+        q = floor((p + r) / 2)
+        Sort(list, p, q)
+        Sort(list, q + 1, r)
+        Merge(list, p, q, r)
+    return list
 
 def main():
-    n = int(input())
-    a = [int(i) for i in input().split()]
-    print(Sort(a))
+    # n = int(input())
+    # a = [int(i) for i in input().split()]
+    a = [5, 2, 4, 6, 1, 3, 2, 6]
+    print(Sort(a, 1, len(a)))
 
 count = 0
 if __name__ == '__main__':
